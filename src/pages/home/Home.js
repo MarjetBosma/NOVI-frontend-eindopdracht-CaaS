aa8a066ac189470cd48514bca40c0767015321df
MarjetBosma <marjet_bosma@hotmail.com>
2023-11-21 17:45:08 +0100

    Nog een tekstuele aanpassing in de Welkom-popup

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -52,13 +52,14 @@ function Home() {
               <div className="welcome-modal">
                   <div className="welcome-modal-content">
                     <h2>Welkom!</h2>
-                    <p>Deze webapplicatie heb ik gemaakt in het kader van de leerlijn Frontend Development, aan de NOVI Hogeschool. Uiteraard ben ik erg trots op het eindresultaat!</p>
-                    <p>Ik heb alleen de "voorkant" hoeven bouwen, dus het deel dat jij als gebruiker ziet. Dat brengt beperkingen met zich mee: omdat er geen echte server aan de "achterkant" aanwezig is, kunnen gegevens niet langdurig worden opgeslagen. Je favoriete kattenplaatjes worden bewaard zolang de applicatie is geopend, en verdwijnen na het verversen van de pagina of het afsluiten van de browser. Je profielfoto verdwijnt zodra je naar een andere pagina binnen deze website gaat. Je gebruikersnaam en wachtwoord blijven actief zolang de applicatie draait, en na afsluiten worden deze nog ongeveer een uur bewaard op een soort simulatieserver. Als je deze website een dag later opnieuw bezoekt, moet je dus eerst jezelf opnieuw registreren.</p>
+                    <p>De CaaS webapplicatie heb ik gemaakt in het kader van de leerlijn Frontend Development, aan de NOVI Hogeschool. Uiteraard ben ik erg trots op het eindresultaat!</p>
+                    <p>Ik heb alleen de "voorkant" hoeven bouwen, dus het deel dat jij als gebruiker ziet. Dat brengt beperkingen met zich mee: omdat er geen echte server aan de "achterkant" aanwezig is, kunnen gegevens niet (langdurig) worden opgeslagen. Als je deze website een tijdje later opnieuw bezoekt, moet je dus eerst jezelf opnieuw registreren.</p>
+                    <p>De kattenplaatjes worden opgehaald van een externe server, en een deel van de code die daarvoor nodig is, is niet in mijn beheer. Als bepaalde onderdelen op de Afbeeldingen-pagina niet (meer) werken, kan dit mogelijk komen doordat er daarin iets veranderd is.</p>
                     <p>Los daarvan is het zeker de moeite waard om de app te bekijken en uit te proberen. Ik hoop dat alle gezellige kattenplaatjes bij jou ook zorgen voor een glimlach!</p>
                     <Button className="close-welcome-modal-button"
                             type="button"
                             clickHandler={closeWelcomeModal}>
-                        Begrepen!
+                        Sluiten
                     </Button>
                   </div>
                 </div>
